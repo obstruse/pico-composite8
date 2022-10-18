21d32a042285cdff77a9e3e118bbb6794e71b2a5
obstruse <neisius2@earthlink.net>
2022-10-17 22:49:48 -0700

    Update cvideo.c

--- cvideo.c
+++ cvideo.c
@@ -13,7 +13,21 @@
  *  Last Updated:       15/02/2021
  *-----------------------------------------------
 */
+/*
+    Copyright (C) 2021 Bill Neisius <obstruse@earthlink.net>
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #define TESTPATTERN
 
